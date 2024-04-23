1ec6d367647e65858b19ebd3fd30d4ec1a019d38
yigit ocak <yigitockk@gmail.com>
2024-04-23 17:36:12 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
