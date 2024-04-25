0485b1e752e60885de3f1b01551a5cbe276170b2
yigit ocak <yigitockk@gmail.com>
2024-04-25 18:21:26 -0400

    delete warehouse and inventory modals

--- client/src/components/WarehouseItem/WarehouseItem.js
+++ client/src/components/WarehouseItem/WarehouseItem.js
@@ -1,11 +1,15 @@
 import './WarehouseItem.scss'
-import { Link } from 'react-router-dom'
 import trash from '../../assets/icons/delete_outline-24px.svg'
 import pencil from '../../assets/icons/edit-24px.svg'
 import sort from '../../assets/icons/sort-24px.svg'
 import arrow from '../../assets/icons/chevron_right-24px.svg'
+import {useState} from "react";
+import {DeleteWarehouseModal} from "../DeleteWarehouseModal/DeleteWarehouseModal";
 
 const WarehouseItem = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const toggleModal = () => setModalOpen(!modalOpen);
     return (
         <>
         <article className='item'>
@@ -34,27 +38,28 @@ const WarehouseItem = () => {
             </section>
 
             <section className='item__actions'>
-                <img src={trash}/>
-                <img src={pencil}/>
+                // TODO: id needs to passed as a prop
+                <button className="item__button" onClick={toggleModal}><img src={trash} alt="Delete"/></button>
+                <img src={pencil} alt="pencil"/>
             </section>
         </article>
 
-        <nav className='warehouseNav'>
+            <nav className='warehouseNav'>
             <div className='warehouseNav__div'>
                 <p className='item__label'>WAREHOUSE</p>
-                <img src={sort}/>
+                <img src={sort} alt="sort"/>
             </div>
             <div className='warehouseNav__div'>
                 <p className='item__label'>ADDRESS</p>
-                <img src={sort}/>
+                <img src={sort} alt="sort"/>
             </div>
             <div className='warehouseNav__div'>
                 <p className='item__label'>CONTACT NAME</p>
-                <img src={sort}/>
+                <img src={sort} alt="sort"/>
             </div>
             <div className='warehouseNav__div'>
                 <p className='item__label'>CONTACT INFORMATION</p>
-                <img src={sort}/>
+                <img src={sort} alt="sort"/>
             </div>
             <div className='warehouseNav__div'>
                 <p className='item__label'>ACTIONS</p>
@@ -64,7 +69,7 @@ const WarehouseItem = () => {
         <article className='otherItem'>
             <div className='warehouseNav__div'>
                 <p className='item__name'>placeholder</p>
-                <img src={arrow}/>
+                <img src={arrow} alt="arrow"/>
             </div>
             <div className='warehouseNav__div'>
                 <p className='item__data'>placeholder</p>
@@ -76,10 +81,12 @@ const WarehouseItem = () => {
                 <p className='item__data'>placeholder</p>
             </div>
             <section className='item__actions'>
-                    <img src={trash}/>
-                    <img src={pencil}/>
+                // TODO: id needs to passed as a prop
+                <button className="item__button" onClick={toggleModal}><img src={trash} alt="Delete"/></button>
+                <img src={pencil} alt="pencil"/>
             </section>
         </article>
+            {modalOpen && <DeleteWarehouseModal warehouseName="placeholder" closeModal={toggleModal} />}
         </>
     )
 }
