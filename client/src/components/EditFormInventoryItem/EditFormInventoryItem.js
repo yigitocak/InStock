87f0aad563c2475e75a7ced9cf6cd13018d10ecc
Harmit <162362409+harmit-s@users.noreply.github.com>
2024-04-26 14:32:31 -0400

    tablet styling done

--- client/src/components/EditFormInventoryItem/EditFormInventoryItem.js
+++ client/src/components/EditFormInventoryItem/EditFormInventoryItem.js
@@ -24,62 +24,66 @@ const EditFormInventoryItem = ({ item }) => {
     return (
         <section className='edit__main'>
             <section className='edit__container'>
-                <section className='edit__item'>
-                    <h2 className='edit__title'>Item Details</h2>
-                    <div className='edit__form'>
-                        <label htmlFor='item-name' className='edit__label'>Item Name</label>
-                        <input id='item-name' className='edit__input' type='text' placeholder={item_name} />
-                    </div>
-                    <div className='edit__form'>
-                        <label htmlFor='description' className='edit__label'>Description</label>
-                        <input id='description' className='edit__input-description' type='text' placeholder={description} />
-                    </div>
-                    <div className='edit__form edit__select-wrapper'>
-                        <label htmlFor='category' className='edit__label'>Category</label>
-                        <select id='category' className='edit__select' value={selectedCategory} onChange={handleCategoryChange}>
-                            <option value="">{item_name}</option>
-                            {category && category.map((cat, index) => (
-                                <option key={index} value={cat}>{cat}</option>
-                            ))}
-                        </select>
-                        <img className='edit__arrowdrop' src={arrowDrop} alt='arrow drop down' />
-                    </div>
-                </section>
-                <div className='edit__border'></div>
-                <section className='edit__availability'>
-                    <h2 className='edit__title'>Item Availability</h2>
-                    <h3 className='edit__label'>Status</h3>
-                    <div className='edit__radio'>
-                        <div className="edit__status-selector">
-                            <input
-                                type="radio"
-                                id="inStock"
-                                value="inStock"
-                                checked={status === 'inStock'}
-                                onChange={() => handleStatusClick('inStock')}
-                            />
-                            <label className='edit__radio-label' htmlFor="inStock">In stock</label>
+                <div className='edit__tablet'>
+                    <section className='edit__item'>
+                        <h2 className='edit__title'>Item Details</h2>
+                        <div className='edit__form'>
+                            <label htmlFor='item-name' className='edit__label'>Item Name</label>
+                            <input id='item-name' className='edit__input' type='text' placeholder={item_name} />
                         </div>
-                        <div className="edit__status-selector">
-                            <input
-                                type="radio"
-                                id="outOfStock"
-                                value="outOfStock"
-                                checked={status === 'outOfStock'}
-                                onChange={() => handleStatusClick('outOfStock')}
-                            />
-                            <label className='edit__radio-label' htmlFor="outOfStock">Out of stock</label>
+                        <div className='edit__form'>
+                            <label htmlFor='description' className='edit__label'>Description</label>
+                            <input id='description' className='edit__input-description' type='text' placeholder={description} />
                         </div>
-                    </div>
-                    <div className='edit__container-2'>
-                        <label className='edit__label' htmlFor='warehouse'>Warehouse</label>
-                        <select id='warehouse' className='edit__select' value={selectedWarehouse} onChange={handleWarehouseChange}>
-                            <option value=""></option>
-                            <option value={warehouse_name}>{warehouse_name}</option>
-                        </select>
-                        <img className='edit__arrowdrop' src={arrowDrop} alt='arrow drop down' />
-                    </div>
-                </section>
+                        <div className='edit__form edit__select-wrapper'>
+                            <label htmlFor='category' className='edit__label'>Category</label>
+                            <select id='category' className='edit__select' value={selectedCategory} onChange={handleCategoryChange}>
+                                <option value="">{item_name}</option>
+                                {category && category.map((cat, index) => (
+                                    <option key={index} value={cat}>{cat}</option>
+                                ))}
+                            </select>
+                            <img className='edit__arrowdrop' src={arrowDrop} alt='arrow drop down' />
+                        </div>
+                    </section>
+                    <div className='edit__border'></div>
+                    <section className='edit__availability'>
+                        <h2 className='edit__title'>Item Availability</h2>
+                        <h3 className='edit__label'>Status</h3>
+                        <div className='edit__radio'>
+                            <div className="edit__status-selector">
+                                <input
+                                    type="radio"
+                                    id="inStock"
+                                    value="inStock"
+                                    checked={status === 'inStock'}
+                                    onChange={() => handleStatusClick('inStock')}
+                                    className='edit__radio-button'
+                                />
+                                <label className='edit__radio-label' htmlFor="inStock">In stock</label>
+                            </div>
+                            <div className="edit__status-selector">
+                                <input
+                                    type="radio"
+                                    id="outOfStock"
+                                    value="outOfStock"
+                                    checked={status === 'outOfStock'}
+                                    onChange={() => handleStatusClick('outOfStock')}
+                                    className='edit__radio-button'
+                                />
+                                <label className='edit__radio-label' htmlFor="outOfStock">Out of stock</label>
+                            </div>
+                        </div>
+                        <div className='edit__container-2'>
+                            <label className='edit__label' htmlFor='warehouse'>Warehouse</label>
+                            <select id='warehouse' className='edit__select' value={selectedWarehouse} onChange={handleWarehouseChange}>
+                                <option value=""></option>
+                                <option value={warehouse_name}>{warehouse_name}</option>
+                            </select>
+                            <img className='edit__arrowdrop' src={arrowDrop} alt='arrow drop down' />
+                        </div>
+                    </section>
+                </div>
                 <section className='edit__buttons'>
                     <button className='edit__button'>Cancel</button>
                     <button className='edit__button edit__button--blue'>Save</button>
