3240505508f183169cecccb4601acaf4454ebec4
Harmit <162362409+harmit-s@users.noreply.github.com>
2024-04-26 20:45:26 -0400

    updates to the form

--- client/src/components/EditFormInventoryItem/EditFormInventoryItem.js
+++ client/src/components/EditFormInventoryItem/EditFormInventoryItem.js
@@ -1,47 +1,64 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './EditFormInventoryItem.scss';
 import arrowDrop from '../../assets/icons/arrow_drop_down-24px.svg';
 
 const EditFormInventoryItem = ({ item }) => {
-    const [selectedCategory, setSelectedCategory] = useState(item.category);
-    const [selectedWarehouse, setSelectedWarehouse] = useState(item.warehouse_name);
-    const [status, setStatus] = useState(item.status);
+    const navigate = useNavigate();
 
-    const { item_name, description, category, warehouse_name } = item;
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    const handleCategoryChange = (event) => {
-        setSelectedCategory(event.target.value);
-    };
+        const form = event.target;
+        const name = form.item_name.value;
+        const description = form.description.value;
+        const category = form.category.value;
+        const quantity = form.quantity.value;
+        const warehouse = form.warehouse_name.value;
 
-    const handleWarehouseChange = (event) => {
-        setSelectedWarehouse(event.target.value);
-    };
+        const itemData = {
+            name,
+            description,
+            category,
+            quantity,
+            warehouse
+        };
 
-    const handleStatusClick = (newStatus) => {
-        setStatus(newStatus);
+        try {
+            const response = await axios.put(`http://localhost:8080/inventories/${item.id}`, itemData);
+
+            if (response.status === 201) {
+                alert("Item successfully Edited!");
+                navigate("/");
+            } else {
+                alert("Failed to edit item. Please try again.");
+            }
+        } catch (error) {
+            console.error("Error editing item:", error);
+            alert("Failed to edit item. Please try again.");
+        }
     };
 
     return (
         <section className='edit__main'>
-            <section className='edit__container'>
+            <form className='edit__container' onSubmit={handleSubmit}>
                 <div className='edit__tablet'>
                     <section className='edit__item'>
                         <h2 className='edit__title'>Item Details</h2>
                         <div className='edit__form'>
                             <label htmlFor='item-name' className='edit__label'>Item Name</label>
-                            <input id='item-name' className='edit__input' type='text' placeholder={item_name} />
+                            <input id='item-name' className='edit__input' type='text' defaultValue={item.item_name} />
                         </div>
                         <div className='edit__form'>
                             <label htmlFor='description' className='edit__label'>Description</label>
-                            <input id='description' className='edit__input-description' type='text' placeholder={description} />
+                            <input id='description' className='edit__input-description' type='text' defaultValue={item.description} />
                         </div>
                         <div className='edit__form edit__select-wrapper'>
                             <label htmlFor='category' className='edit__label'>Category</label>
-                            <select id='category' className='edit__select' value={selectedCategory} onChange={handleCategoryChange}>
-                                <option value="">{item_name}</option>
-                                {category && category.map((cat, index) => (
-                                    <option key={index} value={cat}>{cat}</option>
-                                ))}
+                            <select id='category' className='edit__select' defaultValue={item.category}>
+                                <option value="">{item.item_name}</option>
+                                
                             </select>
                             <img className='edit__arrowdrop' src={arrowDrop} alt='arrow drop down' />
                         </div>
@@ -56,8 +73,7 @@ const EditFormInventoryItem = ({ item }) => {
                                     type="radio"
                                     id="inStock"
                                     value="inStock"
-                                    checked={status === 'inStock'}
-                                    onChange={() => handleStatusClick('inStock')}
+                                    checked={item.status === 'inStock'}
                                     className='edit__radio-button'
                                 />
                                 <label className='edit__radio-label' htmlFor="inStock">In stock</label>
@@ -67,28 +83,33 @@ const EditFormInventoryItem = ({ item }) => {
                                     type="radio"
                                     id="outOfStock"
                                     value="outOfStock"
-                                    checked={status === 'outOfStock'}
-                                    onChange={() => handleStatusClick('outOfStock')}
+                                    checked={item.status === 'outOfStock'}
                                     className='edit__radio-button'
                                 />
                                 <label className='edit__radio-label' htmlFor="outOfStock">Out of stock</label>
                             </div>
                         </div>
+                        {item.status === 'inStock' && (
+                            <div className='edit__quantity'>
+                                <label htmlFor='item-name' className='edit__label'>Quantity</label>
+                                <input id='item-name' className='edit__input-q' type='text' defaultValue={item.quantity} />
+                            </div>
+                        )}
                         <div className='edit__container-2'>
                             <label className='edit__label' htmlFor='warehouse'>Warehouse</label>
-                            <select id='warehouse' className='edit__select' value={selectedWarehouse} onChange={handleWarehouseChange}>
+                            <select id='warehouse' className='edit__select' defaultValue={item.warehouse_name}>
                                 <option value=""></option>
-                                <option value={warehouse_name}>{warehouse_name}</option>
+                                <option value={item.warehouse_name}>{item.warehouse_name}</option>
                             </select>
                             <img className='edit__arrowdrop' src={arrowDrop} alt='arrow drop down' />
                         </div>
                     </section>
                 </div>
                 <section className='edit__buttons'>
-                    <button className='edit__button'>Cancel</button>
+                    <button className='edit__button' onClick={() => navigate("/inventories")}>Cancel</button>
                     <button className='edit__button edit__button--blue'>Save</button>
                 </section>
-            </section>
+            </form>
         </section>
     );
 }
--- client/src/pages/InventoryItemEdit/InventoryItemEdit.js
+++ client/src/pages/InventoryItemEdit/InventoryItemEdit.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import arrowIcon from "../../assets/icons/arrow_back-24px.svg";
 import EditFormInventoryItem from "../../components/EditFormInventoryItem/EditFormInventoryItem";
@@ -7,33 +7,35 @@ import EditFormInventoryItem from "../../components/EditFormInventoryItem/EditFo
 import "./InventoryItemEdit.scss";
 
 const InventoryItemEdit = () => {
-    const { itemId } = useParams();
-    const [inventoryItem, setInventoryItem] = useState({});
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [inventoryItem, setInventoryItem] = useState([]);
   
     useEffect(() => {
       const fetchData = async () => {
         try {
           const inventoryData = await axios.get(
-            `http://localhost:8080/api/inventories/${itemId}`
+            `http://localhost:8080/api/inventories/${id}`
           );
           setInventoryItem(inventoryData.data);
         } catch (error) {
           console.error(error);
         }
       };
+      console.log(inventoryItem);
   
       fetchData();
   
       return () => {
         
       };
-    }, [itemId]);
+    }, [id]);
 
   return (
     <main className="main">
       <section className="main__container">
         <div className="main__top">
-          <img className="main__arrow" src={arrowIcon} alt="arrow icon" />
+          <img className="main__arrow" src={arrowIcon} alt="arrow icon" onClick={() => navigate("/inventories")} />
           <h1 className="main__title">Edit Inventory Item</h1>
         </div>
 
