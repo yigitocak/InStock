08342286bf66360058961e3627c07e484e8fbbcc
Alberto Leong <117922475+albertoleong@users.noreply.github.com>
2024-04-28 17:31:45 -0400

    finally got everything working!!

--- client/src/components/FormAddInventoryItem/FormAddInventoryItem.js
+++ client/src/components/FormAddInventoryItem/FormAddInventoryItem.js
@@ -1,69 +1,86 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './FormAddInventoryItem.scss';
 import arrowDrop from '../../assets/icons/arrow_drop_down-24px.svg';
 
 const FormAddInventoryItem = () => {
+
+    const API_URL = 'http://localhost:8080/api'
+    const navigate = useNavigate()
+
     const [formData, setFormData] = useState({
+        warehouse_id: '',
         item_name: '',
         description: '',
         category: '',
         status: '',
-        quantity: '',
-        warehouseName: ''
-    });
+        quantity: ''
+      });
 
-    const [status, setStatus] = useState('')
-    const navigate = useNavigate();
+      const handleStatusClick = (newStatus) => {
+        setFormData(prevState => ({
+          ...prevState,
+          status: newStatus
+        }));
+      };
 
+    const [categories, setCategories] = useState([]);
+    const [warehouseOptions, setWarehouseOptions] = useState([]);
+    
+    useEffect(() => {
+    const fetchOptions = async () => {
+      try {
+        const response = await axios.get(API_URL + '/inventories')
+        const responseData = response.data
+        const uniqueCategories = [...new Set(responseData.map(obj => obj.category))]
+        setCategories(uniqueCategories)
+
+        const warehouses = await axios.get(API_URL + '/warehouses')
+        const warehousesData = warehouses.data
+        setWarehouseOptions(warehousesData)
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchOptions();
+    }, []);
+    
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+          ...prevState,
+          [name]: value
+        }));
+      };
 
-    const handleStatusClick = (newStatus) => {
-        setStatus(newStatus);
-    };
-
-    const API_URL = "http://localhost:8080/api"
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(API_URL + '/inventories', formData)
-            console.log(response.data)
-            alert('You have added a new item to the inventory!')
-        } catch (error) {
-            console.error(error)
-            alert('Failed to add new item to the inventory.')
-        }
+    const handleWarehouseChange = (e) => {
+        const selectedWarehouseId = e.target.value;
+        setFormData({ ...formData, warehouse_id: selectedWarehouseId });
     };
     
-    const [categoryOptions, setCategoryOptions] = useState([]);
-    const [warehouseOptions, setWarehouseOptions] = useState([]);
-
-    useEffect(() => {
-        const getTheOptions = async () => {
-            try {
-                const options = await axios.get(API_URL + '/inventories')
-                const optionsData = options.data;
-
-                const uniqueCategories = [...new Set(optionsData.map(obj => obj.category))]
-                const categoryOptions = uniqueCategories.map((category, index) => (
-                    <option key={index} value={category}>{category}</option>
-                ));
-                setCategoryOptions(categoryOptions);
-
-                const uniqueWarehouses = [...new Set(optionsData.map(obj => obj.warehouse_name))]
-                const warehouseOptions = uniqueWarehouses.map((warehouse, index) => (
-                    <option key={index} value={warehouse}>{warehouse}</option>
-                ));
-                setWarehouseOptions(warehouseOptions);
-            } catch(error) {
-                console.error(error)
-            }
+    const handleSubmit = async (e) => {  
+        console.log('Form Data:', formData);
+        e.preventDefault();
+        const requiredFields = ['warehouse_id', 'item_name', 'description', 'category', 'status'];
+        const missingFields = requiredFields.filter(field => !formData[field]);
+    
+        if (missingFields.length > 0) {
+            alert('Please fill in all required fields.');
+            return;
         }
-        getTheOptions();
-    }, []);
+
+        try {
+          const response = await axios.post(API_URL + '/inventories', formData);
+          console.log(response.data);
+          alert('You have added a new item to the inventory!')
+          navigate('/inventories');
+        } catch (error) {
+          console.error('Error adding item:', error);
+          alert('Failed to add new item to the inventory.')
+        }
+      };
 
     return (
         <form className='addForm__container' onSubmit={handleSubmit}>
@@ -71,21 +88,35 @@ const FormAddInventoryItem = () => {
                 <section className='addForm__item'>
                     <h2 className='addForm__title'>Item Details</h2>
                     <div className='addForm__form'>
-                        <label htmlFor='item-name' className='addForm__label'>
+                        <label htmlFor='item name' className='addForm__label'>
                             Item Name
                         </label>
-                        <input id='item-name' className='addForm__input' type='text' name='item_name' value={formData.item_name} onChange={handleChange} />
+                        <input className='addForm__input' 
+                            type="text" name="item_name" 
+                            value={formData.item_name} 
+                            onChange={handleChange} />
                     </div>
                     <div className='addForm__form'>
                         <label htmlFor='description' className='addForm__label'>
                             Description
                         </label>
-                        <textarea id='description' className='addForm__input-description' type='text' name='description' value={formData.description} onChange={handleChange} />
+                        <textarea className='addForm__input-description' 
+                            type="text" name="description" 
+                            value={formData.description} 
+                            onChange={handleChange} />
                     </div>
                     <div className='addForm__form addForm__select-wrapper'>
                         <label htmlFor='category' className='addForm__label'>Category</label>
-                        <select id='category' className='addForm__select' name='category' value={formData.category} onChange={handleChange}>
-                            {categoryOptions}
+                        <select className='addForm__select' 
+                            name="category" 
+                            value={formData.category} 
+                            onChange={handleChange}
+                        >
+                            <option value="">Select Category</option>
+                            {categories.map((category, index) => (
+                            <option key={category.id || index} value={category.id}>{category}</option>
+                            ))}
+
                         </select>
                         <img className='addForm__arrowdrop' src={arrowDrop} alt='arrow drop down' />
                     </div>
@@ -99,9 +130,9 @@ const FormAddInventoryItem = () => {
                             <input
                                 type="radio"
                                 id="inStock"
-                                value="inStock"
-                                checked={status === 'inStock'}
-                                onChange={() => handleStatusClick('inStock')}
+                                checked={formData.status === 'in stock'}
+                                onChange={() => handleStatusClick('in_stock')}
+                                name="status"
                                 className='addForm__radio-button'
                             />
                             <label className='addForm__radio-label' htmlFor="inStock">In stock</label>
@@ -110,24 +141,33 @@ const FormAddInventoryItem = () => {
                             <input
                                 type="radio"
                                 id="outOfStock"
-                                value="outOfStock"
-                                checked={status === 'outOfStock'}
-                                onChange={() => handleStatusClick('outOfStock')}
+                                checked={formData.status === 'out of stock'}
+                                onChange={() => handleStatusClick('out_of_stock')}
+                                name="status"
                                 className='addForm__radio-button'
                             />
                             <label className='addForm__radio-label' htmlFor="outOfStock">Out of stock</label>
                         </div>
                     </div>
-                    {status === 'inStock' && (
-                        <div className='addForm__quantity'>
-                            <label htmlFor='quantity' className='addForm__label'>Quantity</label>
-                            <input id='quantity' className='addForm__input-q' type='number' name='quantity' value={formData.quantity} onChange={handleChange} />
-                        </div>
-                    )}
+                    {formData.status === 'in_stock' && (
+                    <div className='addForm__quantity'>
+                    <label htmlFor='quantity' className='addForm__label'>Quantity</label>
+                    <input
+                        className='addForm__input-q'
+                        type="number"
+                        name="quantity"
+                        value={formData.quantity}
+                        onChange={handleChange}
+                    />
+                    </div>
+                )}
                     <div className='addForm__container-2'>
-                        <label htmlFor='warehouseName' className='addForm__label'>Warehouse</label>
-                        <select id='warehouseName' className='addForm__select' name='warehouseName' value={formData.warehouseName} onChange={handleChange} >
-                            {warehouseOptions}
+                        <label htmlFor='warehouse name' className='addForm__label'>Warehouse</label>
+                        <select id='warehouse_name' className='addForm__select' name='warehouse_id' value={formData.warehouse_id} onChange={handleWarehouseChange}>
+                            <option value="">Select Warehouse</option>
+                            {warehouseOptions.map((warehouse) => (
+                                <option key={warehouse.id} value={warehouse.id}>{warehouse.warehouse_name}</option>
+                            ))}
                         </select>
                         <img className='addForm__arrowdrop' src={arrowDrop} alt='arrow drop down' />
                     </div>
